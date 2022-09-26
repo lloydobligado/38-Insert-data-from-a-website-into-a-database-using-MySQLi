d04e58663549b79c3dd6dced2b0bd814f6a8b372
lloydobligado <82791735+lloydobligado@users.noreply.github.com>
2022-09-26 21:02:07 +0800

    create form

--- index.php
+++ index.php
@@ -11,8 +11,24 @@
     <title>Document</title>
 </head>
 <body>
+
+<form action="index.php" method="post">
+<input type="text" name="first" placeholder="First Name" ><br>
+<input type="text" name="last" placeholder="Last Name" ><br>
+<input type="email" name="email" placeholder="Email" ><br>
+<input type="text" name="uid" placeholder="User ID" ><br>
+<input type="password" name="pwd" placeholder="Password" ><br>
+<input type="submit" name="submit" value="SUBMIT">
+</form>
     
 <?php
+
+    $fName = 
+    $lName = 
+    $Email = 
+    $userID = 
+    $password = 
+
     $sql = "SINSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('Daniel', 'Nielsen', 'danielnielsen@gmail.com', 'Admin', 'test123');"; 
     $result = mysqli_query($conn, $sql); 
 ?>
