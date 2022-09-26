4cfe5899a57ff06dca23fc08ba1bad6a2634db1f
lloydobligado <82791735+lloydobligado@users.noreply.github.com>
2022-09-26 21:27:24 +0800

    changes

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,13 +1,12 @@
     
 <?php
+    include_once 'db.inc.php';
 
     $first = $_POST['first'];
     $last = $_POST['last'];
     $email = $_POST['email'];
     $uid = $_POST['uid'];
     $pwd = $_POST['pwd'];
-
-    include_once 'db.inc.php';
 
     $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd');"; 
     $result = mysqli_query($conn, $sql); 
